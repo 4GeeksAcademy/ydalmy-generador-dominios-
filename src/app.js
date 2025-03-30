be5da0922b704ed4361b2e6098f7a43aa41a5558
ydalmy <ydalmi.arvelaez@gmail.com>
2025-03-30 20:44:07 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let articulo = ["the", "de", "for"]
+  let adjetivo = ["good", "nice","bad"]
+  let nombre = ["place", "thing", "weather"]
+
+  let elementoLista = document.getElementById("lista");
+
+  for ( let i=0; i<articulo.length; i++)
+  {
+    for ( let j=0; j<adjetivo.length; j++)
+    {
+      for ( let k=0; k<nombre.length; k++)
+        {
+          console.log(articulo[i] + adjetivo[j] + nombre[k] + '.com');
+          
+          elementoLista.innerHTML += "<li>" + articulo[i] + adjetivo[j] + nombre[k] + '.com' + "</li>";
+        }
+    }
+  }
+  
+  
 };
